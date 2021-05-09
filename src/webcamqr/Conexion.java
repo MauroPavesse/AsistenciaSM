c9575d8edd233c1622f15f030890e46425a5e4e9
Usuario <Usuario@DESKTOP-1UCT313>
2021-05-09 01:06:02 -0300

    Version prueba

---
+++ src/webcamqr/Conexion.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webcamqr;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Seba
+ */
+public class Conexion {
+     private static Connection cnx = null;
+
+    public static Connection obtener() throws SQLException, ClassNotFoundException {
+        if (cnx == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                cnx = DriverManager.getConnection("jdbc:mysql://localhost:3306/sistema_presentes", "root", "sebjjk02");
+            } catch (SQLException ex) {
+                throw new SQLException(ex);
+            } catch (ClassNotFoundException ex) {
+                throw new ClassCastException(ex.getMessage());
+            }
+        }
+        return cnx;
+    }
+
+    public static void cerrar() throws SQLException {
+        if (cnx != null) {
+            cnx.close();
+        }
+    }
+}
